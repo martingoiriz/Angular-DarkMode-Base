31e3d634ef715d0689e4015c43dce61801ea1be8
Martin Goiriz <martingoiriz@gmail.com>
2020-10-23 12:41:33 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'dark-mode-base';
+  title = "dark-mode-base";
+
+  theme: boolean = false;
+
+  changeMode() {
+    this.theme = !this.theme;
+  }
 }
